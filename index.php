14aac63e951da1b97b90a6d51fa1ee50d9af5c78
jquerido <36389394+jquerido@users.noreply.github.com>
2018-02-11 20:16:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--<link rel="stylesheet" type="text/css" href="css/group27_css.css"/>-->
+	
+	<!--
+	File: index.php
+	Author: Jessica Querido
+	Date: 02/11/2018
+	Description: Index page for MODE-4202.
+	-->
+	
+	<title>Index</title>
+	
+</head>
+<body>
+	<p>this is a test</p>
+
+</body>
+</html>
